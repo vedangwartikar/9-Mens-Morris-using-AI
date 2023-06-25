0af13516f0a80cae24fccabbd3938c4507a91b3c
Vedang Wartikar <vedangwartikar17@gmail.com>
2023-06-25 12:47:32 -0500

    conert remaining pseudo codes to func()

--- main.py
+++ main.py
@@ -1,25 +1,7 @@
 from textwrap import dedent
 
-# Draw the board
-def draw(board):
-    print(dedent(f"""\
-        {board[18]}--------{board[19]}--------{board[20]}
-        |        |        |
-        |  {board[15]}-----{board[16]}-----{board[17]}  |
-        |  |     |     |  |
-        |  |  {board[12]}--{board[13]}--{board[14]}  |  |
-        |  |  |     |  |  |
-        {board[6]}--{board[7]}--{board[8]}     {board[9]}--{board[10]}--{board[11]}
-        |  |  |     |  |  |
-        |  |  {board[4]}-----{board[5]}  |  |
-        |  |           |  |
-        |  {board[2]}-----------{board[3]}  |
-        |                 |
-        {board[0]}-----------------{board[1]}\
-        """))
-
 # Return the neighbors of a given position
-def neighbors(location):
+def neighbors(location) -> list:
     if location == 0:
         return [1, 6]
     elif location == 1:
@@ -62,11 +44,10 @@ def neighbors(location):
         return [16, 18, 20]
     elif location == 20:
         return [11, 19]
-    else:
-        return []
+    return []
 
 # Return True if the location closes a mill else False
-def closeMill(location, board):
+def closeMill(location, board) -> bool:
     check_mill = board[location]
     if board[location] in ('W', 'B'):
         if location == 0:
@@ -115,7 +96,111 @@ def closeMill(location, board):
             return False
     return False     
 
+def generate_remove(board, move_list) -> list:
+    no_positions_added = True
+    for location, piece in enumerate(board):
+        if piece == 'B':
+            if not closeMill(location, board):
+                temp_board = list(board)
+                temp_board[location] = 'x'
+                temp_board = ''.join(temp_board)
+                move_list.append(temp_board)
+                no_positions_added = False
+    if no_positions_added:
+        move_list.append(board)
+    return move_list
+
+def generate_move(board) -> list:
+    move_list = list()
+    for location, piece in enumerate(board):
+        if piece == 'W':
+            neighbors_list = neighbors(location)
+            for neighbor in neighbors_list:
+                if board[neighbor] == 'x':
+                    temp_board = list(board)
+                    temp_board[location], temp_board[neighbor] = 'x', 'W'
+                    temp_board = ''.join(temp_board)
+                    if closeMill(neighbor, temp_board):
+                        generate_remove(temp_board, move_list)
+                    else:
+                        move_list.append(temp_board)
+    return move_list
+
+def generate_add(board) -> list:
+    add_list = list()
+    for location, piece in enumerate(board):
+        if piece == 'x':
+            temp_board = list(board)
+            temp_board[location] = 'W'
+            temp_board = ''.join(temp_board)
+            if closeMill(location, temp_board):
+                generate_remove(temp_board, add_list)
+            else:
+                add_list.append(temp_board)
+    return add_list
+
+def generate_moves_opening(board) -> str:
+    return generate_add(board)
+
+def generate_hopping(board) -> list:
+    hop_list = list()
+    for location_i, piece_i in enumerate(board):
+        if piece_i == 'W':
+            for location_j, piece_j in enumerate(board):
+                if piece_j == 'x':
+                    temp_board = list(board)
+                    temp_board[location_i], temp_board[location_j] = 'x', 'W'
+                    temp_board = ''.join(temp_board)
+                    if closeMill(location_j, temp_board):
+                        generate_remove(temp_board, hop_list)
+                    else:
+                        hop_list.append(temp_board)
+    return hop_list
+
+def generate_moves_midgame_endgame(board) -> str:
+    if board.count('W') == 3:
+        return generate_hopping(board)
+    else:
+        return generate_move(board)
+    
+def static_estimation_opening(board) -> int:
+    white_pieces, black_pieces = board.count('W'), board.count('B')
+    return white_pieces - black_pieces
+
+def static_estimation_midgame_endgame(board) -> int:
+    white_pieces, black_pieces = board.count('W'), board.count('B')
+    black_moves = len(generate_moves_midgame_endgame(board))
+    if black_pieces <= 2:
+        return 10000
+    elif white_pieces <= 2:
+        return -10000
+    elif black_moves == 0:
+        return 10000
+    else:
+        return 1000 * (white_pieces - black_pieces) - black_moves
+
+# Draw the board
+def draw(board) -> None:
+    print(dedent(f"""\
+        {board[18]}--------{board[19]}--------{board[20]}
+        |        |        |
+        |  {board[15]}-----{board[16]}-----{board[17]}  |
+        |  |     |     |  |
+        |  |  {board[12]}--{board[13]}--{board[14]}  |  |
+        |  |  |     |  |  |
+        {board[6]}--{board[7]}--{board[8]}     {board[9]}--{board[10]}--{board[11]}
+        |  |  |     |  |  |
+        |  |  {board[4]}-----{board[5]}  |  |
+        |  |           |  |
+        |  {board[2]}-----------{board[3]}  |
+        |                 |
+        {board[0]}-----------------{board[1]}\
+        """))
+
+
 if __name__ == "__main__":
-    board = 'xxxxWWWxBxxxWxxBxWxBB'
-    draw(board)
+    board = 'WxxWxxWxxxxxxxxxxxxxx'
+    print(len(generate_add(board)))
+    for x in generate_remove(board):
+        print(x)
     
